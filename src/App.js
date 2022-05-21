730126640d81391a1ffa3acd973f976cbd2da304
akhil <akhilr37@gmail.com>
2022-05-21 11:57:54 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import {Heading, CustomButton} from './styledComponents'
+
+const App = () => (
+  <>
+    <Heading>Hello akhil</Heading>
+    <CustomButton type="button">clickMe</CustomButton>
+    <CustomButton type="button" outline>
+      clickMe
+    </CustomButton>
+  </>
+)
+export default App
